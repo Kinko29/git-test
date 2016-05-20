26f229d46814fa692e70b58b6ee85872e52e764d
Daniel Buecheler <dbahn25@t-online.de>
2016-05-20 17:41:32 +0200

    Erster Commit

---
+++ Main.java
@@ -0,0 +1,35 @@
+package turtle;
+import java.awt.Color;
+import java.awt.geom.Point2D.Double;
+
+import ch.aplu.turtle.*;
+
+public class Main {
+    
+    static Turtle t;
+
+    public static void main(String[] args) {
+        t = new Turtle();
+        t.hideTurtle();
+        tree(64);
+    }
+    
+    
+    static void tree(double l) {
+        if(l < 3)
+            return;
+        t.fd(l);
+        Double pos = t.getPos();
+        // right branch
+        t.rt(45);
+        tree(l/2);     
+        
+        // left branch
+        t.setPos(pos);
+        t.lt(90);
+        tree(l/2);
+        
+        t.rt(45);
+    }
+
+}
